3d94da9aa4affca57a52d382f9c28cde0d358c09
ReZa ZaRe <rz.zare@gmail.com>
2020-01-01 16:17:23 +0330

    update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-import setuptools
-from setuptools import setup
-from .user_operation import __version__
+from setuptools import setup, find_packages
+from user_operation import __version__
 
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+requires = ["pod-base>=1,<2"]
 
 setup(
     name="pod-user-operation",
@@ -17,13 +18,19 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     keywords=["pod", "user operation", "pod sdk"],
-    packages=setuptools.find_packages(exclude=("build", "dist")),
-    install_requires=[
-        "pod-base>=1,<2"
-    ],
+    packages=find_packages(),
+    install_requires=requires,
+    zip_safe=False,
     classifiers=[
-        "Natural Language :: English",
         "Natural Language :: Persian",
+        "Natural Language :: English",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
     ],
     python_requires='>=2.7',
     package_data={
